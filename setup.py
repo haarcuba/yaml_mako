5cfbee584a7ac3a0fe7344401c01d67ede2532a0
Yoav Kleinberger <haarcuba@gmail.com>
2017-01-23 01:03:02 +0200

    setup script

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+README = 'use mako templating in your YAML files'
+
+requires = [
+        'pyyaml',
+        'mako',
+        ]
+
+setup(name='yaml-mako',
+      version='0.1.0',
+      description=README,
+      long_description=README,
+      url='https://github.com/haarcuba/yaml-mako',
+      classifiers=[
+          "Programming Language :: Python",
+      ],
+      author='Yoav Kleinberger',
+      author_email='haarcuba@gmail.com',
+      keywords='yaml, mako, templating',
+      packages=find_packages(),
+      py_modules = [ "yaml_mako" ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      )
